6488346dcf72294aa6a1756ce442775030f557d6
Mike Pitts <mikepitts25@gmail.com>
2021-07-02 00:17:18 -0400

    Some Input Validation
    While still not complete, the program should now validate some input. TODO: Need to catch Exceptions. Need to attempt socket creation.

--- PortScanner/scanner.py
+++ PortScanner/scanner.py
@@ -1,17 +1,38 @@
 import sys
 
+
+def bye():
+    print("Please enter a valid IPv4 address as an argument!")
+    exit()
+
+
 n = len(sys.argv)
 
 if n < 2:
-    print("Please enter a valid IP address as an argument!")
-    exit()
+    bye()
 
 target = sys.argv[1]
-
 print("\nTarget entered: {0}".format(target))
 
+if target.count('.') != 3:
+    bye()
+else:
+    octets = target.split('.')
+
+for num in octets:
+    numInt = int(num)
+    if numInt <= 0 or numInt > 255:
+        bye()
 
 
-def scanner(target):
-    for octet in range(1, 25): #change to 256
-        print("Scanning 192.168.4.{0}".format(octet))
+def scanner(ip):
+    ports = input("Enter the port or port range to scan (Ex. 80 or 1-1000: ")
+
+    if ports.count('-') == 1:
+        for port in range(int(ports.split('-')[0]), int(ports.split('-')[1])+1):
+            print("Scanning {0}:{1}".format(ip, port))
+    elif int(ports) >= 0 and int(ports) <= 65535:
+        print("Scanning {0}:{1}".format(ip, ports))
+
+
+scanner(target)
