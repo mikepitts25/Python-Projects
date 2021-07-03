0964048f7db134ede8bff6ff41e6a77901f3981a
Mike Pitts <mikepitts25@gmail.com>
2021-07-03 00:39:01 -0400

    Update bannergrab.py
    Using functions for cleaner code, added timeout and ConnectionRefused exception handling

--- bannerGrabber/bannergrab.py
+++ bannerGrabber/bannergrab.py
@@ -1,11 +1,20 @@
 import socket
 
-s = socket.socket()
 
-ip = input('Please enter the IP: ')
-port = input('Please enter the port: ')
+def grabber(ip, port):
+    s = socket.socket()
+    try:
+        s.connect((ip, int(port)))
+        s.settimeout(3)
+        print("Target is running: " + str(s.recv(1024)).strip('b\'\\r\\n'))
+    except ConnectionRefusedError:
+        print("Connection Timed Out...")
 
-s.connect((ip, int(port)))
+def main():
+    # ip = input('Please enter the IP: ')
+    ip = '192.168.235.128'
+    # port = input('Please enter the port: ')
+    port = 22
+    grabber(ip, port)
 
-print(s.recv(1024))
-
+main()
