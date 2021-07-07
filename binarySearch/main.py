f70008accde3d158c4c0acb492e37645da352ff2
Mike Pitts <mikepitts25@gmail.com>
2021-07-06 20:00:23 -0400

    Update main.py

--- binarySearch/main.py
+++ binarySearch/main.py
@@ -1,12 +1,12 @@
 low = 1
 high = 1000
-answer = 500
+answer = 101
 
 print("Think of a number between {} and {}.".format(low, high))
 input("Press ENTER to start.")
 
 guesses = 1
-while True:
+while low != high:
     guess = low + (high - low) // 2
     """high_low = input("My guess is {}. Should I guess (h)igher, (l)ower or was I "
                      "(c)orrect? Enter h, l, or c to continue.".format(guess).casefold())
@@ -34,3 +34,6 @@ while True:
         print("I got it in {} guesses!".format(guesses))
         break
     guesses += 1
+else:
+    print("Your number was {}".format(low))
+    print("I got it in {} guesses!".format(guesses))
